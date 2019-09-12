02a60afddb3adfbc05a1e62b338338e95f47c208
MindyV21 <43157261+MindyV21@users.noreply.github.com>
2019-09-12 17:12:33 -0500

    starting new drive forward method

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveForwardV.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "driveForwardV", group = "auto")
+//@Disabled
+
+public class driveForwardV extends SkystoneLinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        init(hardwareMap, false);
+
+        waitForStart();
+
+        double distance = 0;
+        
+
+
+    }
+}
+
