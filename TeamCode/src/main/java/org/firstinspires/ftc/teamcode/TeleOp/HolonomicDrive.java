7061e180ddafb22518eb7c93590759ef48729b49
Jerry Luo <jerryluoaustin@gmail.com>
2019-10-21 17:52:15 -0500

    holonomic drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/HolonomicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/HolonomicDrive.java
@@ -1,17 +1,18 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.SkystoneLinearOpMode;
 
+@TeleOp(name="HolonomicDrive", group="teleop")
+//@Disabled
 public class HolonomicDrive extends SkystoneLinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         init(hardwareMap, true);
-
-
 
         double xAxis = -gamepad1.left_stick_x, yAxis = -gamepad1.left_stick_y, zAxis = -gamepad1.right_stick_x;
 
@@ -27,7 +28,7 @@ public class HolonomicDrive extends SkystoneLinearOpMode {
 
             double lfPower = yAxis+xAxis-zAxis, rfPower = yAxis-xAxis+zAxis, lbPower = yAxis-xAxis-zAxis, rbPower = yAxis+xAxis+zAxis;
 
-            if (Math.abs(gamepad1.left_stick_y) > 0.05 || Math.abs(gamepad1.left_stick_y) < -0.05) {
+            if (Math.abs(gamepad1.left_stick_y) > 0.05) {
                 LF.setPower(Range.clip(lfPower, -1, 1));
                 RF.setPower(Range.clip(rfPower, -1, 1));
                 LB.setPower(Range.clip(lbPower, -1, 1));
