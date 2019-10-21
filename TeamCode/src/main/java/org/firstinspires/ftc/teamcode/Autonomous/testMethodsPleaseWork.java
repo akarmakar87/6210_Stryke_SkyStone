cee1e603e01ff48b21392c155f2b3fc7e95a5a61
MindyV21 <43157261+MindyV21@users.noreply.github.com>
2019-10-21 17:53:10 -0500

    testing methods and teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/testMethodsPleaseWork.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.vuforia.CameraDevice;
+
+
+import org.firstinspires.ftc.teamcode.SkystoneLinearOpMode;
+
+@Autonomous(name="pray to all the gods", group = "auto")
+
+//@Disabled
+public class testMethodsPleaseWork extends SkystoneLinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        init(hardwareMap, true);
+
+        waitForStart();
+
+        //turnPID(90, 0, 0, 0, 10000);
+
+        //sleep(1000);
+
+        //driveDistance(0.5, 10);
+
+        //telemetry.addData("drive forward", "done");
+        //telemetry.update();
+
+        //sleep(1000);
+
+        //strafeDistance(0.5, 10, false);
+
+        //telemetry.addData("strafe right", "done");
+        //telemetry.update();
+
+        //driveForward(-32,30,0.5, -90); // test vuforia PID and drive forward
+
+        driveForward(-32, 50, -0.5, -90); // test drive backwards
+
+        telemetry.addData("strafe right", "done");
+        telemetry.update();
+    }
+}
