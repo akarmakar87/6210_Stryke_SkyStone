032bd89245ed31db21ec4ac3f076075c93e869a8
Garrett Stevenson <garrettmstevenson@gmail.com>
2019-11-30 16:29:43 -0600

    Foundation works!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AML2/foundationRed2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AML2/foundationRed2.java
@@ -16,30 +16,42 @@ public class foundationRed2 extends SkystoneLinearOpMode {
 
         waitForStart();
 
-        strafeAdjust(0.6, 30, false);//Strafe in front of foundation
+        driveDistance(-0.4, 20);    //align with foundation
 
-        driveDistance(-0.6, 50);    //Drive to foundation
+        turnPID(90, 0.7/90, 0.004, 1, 5000);   //turn toward foundation
 
-        foundationD(true);  //Grab foundation
+        driveDistance(-0.4, 39);    //Drive to foundation
 
-        sleep(1500);
+        foundationD(false);  //Grab foundation
 
-        driveDistance(1, 72);     //Drag foundation to build site
+        sleep(1000);
 
-        turnPID(-90, 0.6, 0, 2, 2000);   //Rotate robot (w/ foundation) toward the wall
+        setMotorPowers(0.2, 0.5);
+        sleep(500);
+        stopMotors();
 
-        driveDistance(-0.6, 6);     //Push foundation against the wall
+        //turnPID(265,0.7/195, 0.004, 1, 2000);   //angle robot toward wall
 
-        foundationD(false);     //Release foundation
+        //driveDistance(0.4, 20);     //Drag foundation to wall at an angle
 
-        driveDistance(0.6, 6);      //Move from foundation
+        //turnPID(180, 0.7/180, 0.004, 1, 2000);   //set robot to face directly away from the wall
 
-        turnPID(0, 0.6, 0, 2, 2000);   //Align to zero
+        setMotorPowers(0.6, 0.2);
+        sleep(500);
+        stopMotors();
 
-        driveDistance(0.6, 6);     //Toward wall (forward)
+        driveDistance(1, 32);     //Pull foundation into building site
 
-        turnPID(90,0.6,0,2,2000);      //Turn toward alliance bridge
+        //strafeAdjust(0.4, 30, false);
 
-        driveDistance(0.6, 45);     //Drive under alliance bridge
+        turnPID(180, 0.7/90, 0.004, 1, 5000);
+
+        driveDistance(-0.8,15);
+
+        foundationD(true);     //Release foundation
+
+        sleep(1000);
+
+        driveDistance(0.4, 75);     //Drive to the parking spot (forward)
     }
 }
