f0d7f3779af52b1c34f5362d68e18caa7a240000
Asha Karmakar <30420654+akarmakar87@users.noreply.github.com>
2020-01-21 21:25:06 -0600

    red double skystone auto adjustments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AML3/RedDoubleSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AML3/RedDoubleSkystone.java
@@ -65,7 +65,7 @@ public class RedDoubleSkystone extends SkystoneLinearOpMode{
                 driveAdjust(180, -0.8, longAdjust + 38, 3);
                 break;
             case 0:
-                driveAdjust(180, -0.8, longAdjust + 39, 3);
+                driveAdjust(180, -0.8, longAdjust + 40, 3); //originally 39
                 break;
             case 1:
                 driveAdjust(180, -0.8, longAdjust + 49, 3);
@@ -88,15 +88,17 @@ public class RedDoubleSkystone extends SkystoneLinearOpMode{
         // MOVE TO OTHER SIDE
         switch (pos) {
             case -1:
+                //turnPID(175, 0.6/360,0.001,2,3000);
                 turnPID(175, 0.6/360,0.001,2,3000);
-                driveAdjust(175, 1, longAdjust + 53, 3000); // MOVE OTHER SIDE
+                driveAdjust(173, 1, longAdjust + 53, 3000); // MOVE OTHER SIDE
                 break;
             case 0:
                 turnPID(175, 0.6/360,0.001,2,3000);
                 driveAdjust(175, 1, longAdjust + 50, 3000); // MOVE OTHER SIDE
                 break;
             case 1:
-                turnPID(-178, 0.6/360,0.001,2,3000);
+                //turnPID(-178, 0.6/360,0.001,2,3000);
+                turnPID(-176, 0.6/360,0.001,2,3000);
                 driveAdjust(182, 1, longAdjust + 40, 3000); // MOVE OTHER SIDE
                 break;
         }
@@ -105,54 +107,7 @@ public class RedDoubleSkystone extends SkystoneLinearOpMode{
 
         driveAdjust(180, -0.8, 30, 2); //park
 
-        strafeAdjust(.6,9,180,false);
-
-        //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+ old stuffing
-        /*
-        foundationD(true); // drop stone
-
-        turnPID(180, 0.6/360,0.001,2,3000); // autocorrect angle to account for stone friction
-
-        strafeAdjust(0.6,20,-90,false);
-
-        driveAdjust(180, -0.6, 20, 3000); // MOVE BACK TO STONE SIDE
-
-        turnPID(180, 0.8/180, 0.004, 1, 5000); //TURN 90 TO FACE PARK
-
-        //turnPIDtest(180,0.5/90,0,1,5000);
-
-        driveAdjust(180,-0.4, 68+adjust, 7); //CROSS PARK LINE (adjust distance)
-
-        foundationD(true); //RELEASE BLOCK
-
-        //turnPID(180, 0.7/180, 0.004, 1, 5000);
-
-        //driveDistance(0.2, 105+adjust); //second stone (adjust distance) (35 = 11+24)
-
-        //driveDistance(0.6, 108+adjust);
-        driveAdjust(180,0.6, longAdjust,7);
-
-        if(pos == -1)
-            turnPID(280, 0.7/275, 0.004, 1, 5000); //TURN 90 TO FACE STONES
-        else
-            turnPID(270,0.7/270, 0.004, 1,5000);
-        //turnPIDtest(270,0.5/90,0,1,5000);
-
-        driveAdjust(270,-0.6,23, 7); //MOVE forward
-
-        grabStone(pos, true); //GRAB SKYSTONE
-
-        driveAdjust(270,0.9,16, 7); //MOVE back
-
-        turnPID(188, 0.7/90, 0.004, 1, 5000); //TURN 90 TO FACE PARK
-
-        driveAdjust(180,-0.6, longAdjust - 1, 7); //second stone (adjust distance) (35 = 11+24)
-
-        foundationD(true); //RELEASE BLOCK
-
-        driveAdjust(180,0.9,20, 7); //MOVE back
-
-         */
+        strafeAdjust(.6,13,180,false);
 
         telemetry.addData("auto:", "complete");
         telemetry.update();
