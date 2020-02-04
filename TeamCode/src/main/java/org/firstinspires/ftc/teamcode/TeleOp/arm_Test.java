4daa05c09fadac6172a0f50a27c48eb72514d7f3
Garrett Stevenson <garrettmstevenson@gmail.com>
2020-02-04 08:10:46 -0600

    wheel test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/arm_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/arm_Test.java
@@ -9,33 +9,56 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.SkystoneLinearOpMode;
 
-@TeleOp(name="arm_Test", group="auto")
+@TeleOp(name="wheel test", group="teleop")
 //@Disabled
 public class arm_Test extends SkystoneLinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        init(hardwareMap, true);
+        init(hardwareMap, false);
+
+
 
         telemetry.addData("Mode: ", "Waiting for start");
 
+        RB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        RB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
         waitForStart();
 
-        intakeL.setPower(1);
-        intakeR.setPower(-1);
+        while (opModeIsActive() && !isStopRequested()) {
 
-        arm.setTargetPosition(-540);
 
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            if(gamepad1.y)
+            {
+                setMotorPowers(0.3, 0.3);
+            }else if(gamepad1.a)
+            {
+                setMotorPowers(-1, -1);
+            }
+            else if(gamepad1.x)
+            {
+                setStrafePowers(1, false);
+            }
+            else if(gamepad1.b)
+            {
+                setStrafePowers(1, true);
+            }
+            else    setMotorPowers(0, 0);
 
-        arm.setPower(.8);
-
-        //rotate.setPosition(0.5);
-        //setArmPosition(100);
-        sleep(8000);
-        /*setArmPosition(false);
-        sleep(3000);
-        setArmPosition(false);
-        sleep(2000);*/
+            telemetry.addData("RF encoder:" , RF.getCurrentPosition());
+            telemetry.addData("RB encoder:" , RB.getCurrentPosition());
+            telemetry.addData("LF encoder:" , LF.getCurrentPosition());
+            telemetry.addData("LB encoder:" , LB.getCurrentPosition());
+            telemetry.update();
+        }
     }
 }
