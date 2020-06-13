381ed3a8e873af30c4db54b0c3d8495e9b4402c2
Garrett Stevenson <garrettmstevenson@gmail.com>
2020-06-13 15:27:00 -0500

    TEleop explaination in template

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/X_PracticePrograms/TeleOpTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/X_PracticePrograms/TeleOpTemplate.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.X_PracticePrograms;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.SkystoneLinearOpMode;
 
@@ -23,6 +24,21 @@ public class TeleOpTemplate extends SkystoneLinearOpMode {
         while (opModeIsActive() && !isStopRequested()) {
 
             //WRITE IF STATEMENTS W/ GAMEPAD ELEMENTS
+            if(Math.abs(gamepad1.left_stick_y) > 0.05 || Math.abs(gamepad1.right_stick_y) > 0.05)
+            {
+                setMotorPowers(gamepad1.left_stick_y, gamepad1.right_stick_y);
+            }
+
+            if(ifPressed(gamepad2.a))
+            {
+                setClawPosition(true);
+            }
+
+            if(ifPressed(gamepad2.b))
+            {
+                setClawPosition(false);
+            }
+
 
             //GIVE TELEMETRY FOR MOTOR POWERS (to the 2nd decimal)
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
