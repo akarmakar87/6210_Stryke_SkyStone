6f669dbf56163daaac618e655dd489cb407056bc
Lucas Jackson <lucas.c.jackson@gmail.com>
2020-06-27 15:09:26 -0500

    Create LucasJackson_DriveDistancePractice.java
    This is my driveDistance practice.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/X_PracticePrograms/Assignments/LucasJackson_DriveDistancePractice.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.X_PracticePrograms.Assignments;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.SkystoneLinearOpMode;
+
+public class LucasJackson_DriveDistancePractice extends SkystoneLinearOpMode {
+
+    private void driveDistance(double power, double distance, int timeout) {
+
+        double encodedDistance = distance * encoderToInches;
+        ElapsedTime et = new ElapsedTime();
+        et.reset();
+        resetEncoders();
+        setMotorPowers(power, power);
+
+        while (opModeIsActive() && et.seconds() < timeout && getEncoderAvg() <= encodedDistance ) {
+        }
+
+        setMotorPowers(0, 0);
+
+    }
+
+}
